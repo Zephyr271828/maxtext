b0eb0e66eba11b7c9190ebbc0ef673bcd4a38738
yx3038@nyu.edu <yx3038@nyu.edu>
2025-08-26 11:41:49 +0000

    fixed scripts

--- scripts/llama3.1_4b_width_S50.sh
+++ scripts/llama3.1_4b_width_S50.sh
@@ -12,17 +12,17 @@ for var in "${required_vars[@]}"; do
   fi
 done
 
-export MODEL_NAME='llama3-8b'
+export MODEL_NAME='llama3.1-4b-width'
 export NUM_STEPS=12500
 export SEQ_LEN=8192
-export BATCH_SIZE=4
-export GRAD_ACCUM=1
-export LR=3.e-4
+export BATCH_SIZE=2
+export GRAD_ACCUM=4
+export LR=1.e-4
 export MIN_LR_RATIO=0.1
 export WARMUP_RATIO=0.05
 export ASYNC_CHECKPOINTING=false
 export BASE_OUTPUT_DIRECTORY="gs://$BUCKET_NAME/model_ckpts/maxtext"
-export DATA_FILES='/home/zephyr/gcs-bucket/datasets/dclm/llama3_256_arrayrecord/*.array_record'
+export DATA_FILES='/home/zephyr/gcs-bucket/datasets/dclm/llama3_64_array_record/*.array_record'
 
 export RUN_NAME="${MODEL_NAME}_S50_seqlen_${SEQ_LEN}_bs_${BATCH_SIZE}_grad_accum_${GRAD_ACCUM}_lr_${LR}_min_lr_ratio_${MIN_LR_RATIO}_warmup_ratio_${WARMUP_RATIO}"
 
