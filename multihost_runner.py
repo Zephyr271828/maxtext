9a4ecf7d5eb1bd423970a962c8faf9837ebea530
Yufeng Xu <yx3038@nyu.edu>
2025-08-29 13:56:42 -0400

    monkey patched user to make multihostrunner works

--- multihost_runner.py
+++ multihost_runner.py
@@ -210,11 +210,10 @@ def scps(slices, run_name_dir, zip_name):
   commands = []
   worker_list = []
   for cur_slice in slices:
-    cur_slice.name = cur_slice.name.replace('root', 'zephyr')
     for worker_num in range(cur_slice.num_workers):
       command = [
           "gcloud", "compute", "tpus", "tpu-vm", "scp", f"--worker={worker_num}", zip_path,
-          f"{cur_slice.name}:/home/zephyr/", "--ssh-key-file=/home/zephyr/.ssh/id_rsa", "--strict-host-key-checking=no", f"--project={args.PROJECT}", f"--zone={args.ZONE}"
+          f"{cur_slice.name.replace('root', 'zephyr')}:/home/zephyr/", "--ssh-key-file=/home/zephyr/.ssh/id_rsa", "--strict-host-key-checking=no", f"--project={args.PROJECT}", f"--zone={args.ZONE}"
       ]
       if args.INTERNAL_IP:
         command.append("--internal-ip")
@@ -256,7 +255,7 @@ def execute_main_command(main_command, slices, local_log_dir, zip_name):
         remote_command_list = [cd_command, write_kill_script_command , kill_existing_command , main_command]
       remote_command_list_str = " && ".join(remote_command_list)
       gcloud_command=[
-          "gcloud", "alpha", "compute", "tpus", "tpu-vm", "ssh", cur_slice.name, f"--worker={worker_num}",
+          "gcloud", "alpha", "compute", "tpus", "tpu-vm", "ssh", cur_slice.name.replace('root', 'zephyr'), f"--worker={worker_num}",
           "--command", remote_command_list_str, "--ssh-key-file=/home/zephyr/.ssh/id_rsa", "--strict-host-key-checking=no",
           f"--project={args.PROJECT}", f"--zone={args.ZONE}"]
       if args.INTERNAL_IP:
