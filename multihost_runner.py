d522050500200dac6fdcd6fe62c692c0b00988e2
Yufeng Xu <yx3038@nyu.edu>
2025-08-30 14:24:58 -0400

    fixed multihost_runner

--- multihost_runner.py
+++ multihost_runner.py
@@ -213,7 +213,7 @@ def scps(slices, run_name_dir, zip_name):
     for worker_num in range(cur_slice.num_workers):
       command = [
           "gcloud", "compute", "tpus", "tpu-vm", "scp", f"--worker={worker_num}", zip_path,
-          f"{cur_slice}:/home/zephyr/", "--ssh-key-file=/home/zephyr/.ssh/id_rsa", "--strict-host-key-checking=no", f"--project={args.PROJECT}", f"--zone={args.ZONE}"
+          f"{cur_slice.name}:/home/zephyr/", "--ssh-key-file=/home/zephyr/.ssh/id_rsa", "--strict-host-key-checking=no", f"--project={args.PROJECT}", f"--zone={args.ZONE}"
       ]
       if args.INTERNAL_IP:
         command.append("--internal-ip")
