cead97bbdaac13af7bd5ce05cfd2529c188506f5
Yufeng Xu <yx3038@nyu.edu>
2025-09-11 15:02:35 -0400

    restored lm-eval-harness

--- scripts/llama3.1_4b_depth_S50_3e-4_warmup_0.05.sh
+++ scripts/llama3.1_4b_depth_S50_3e-4_warmup_0.05.sh
@@ -15,9 +15,9 @@ done
 export MODEL_NAME='llama3.1-4b-depth'
 export NUM_STEPS=12500
 export SEQ_LEN=8192
-export BATCH_SIZE=2
-export GRAD_ACCUM=4
-export GRAD_CLIP=0.1
+export BATCH_SIZE=1
+export GRAD_ACCUM=1
+export GRAD_CLIP=1.0
 export LR=3.e-4
 export MIN_LR_RATIO=0.1
 export WARMUP_RATIO=0.05
