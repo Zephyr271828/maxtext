a9958093800982f64144dd75192002eb85272b4f
Yufeng Xu <yx3038@nyu.edu>
2025-09-11 10:41:33 -0400

    update grad clip

--- pretrain/llama3_8b_L200_3e-4.sh
+++ pretrain/llama3_8b_L200_3e-4.sh
@@ -12,80 +12,52 @@ for var in "${required_vars[@]}"; do
   fi
 done
 
-
-export MODEL_NAME='llama3-8b'
+export MODEL_NAME='llama3.1-8b'
 export NUM_STEPS=50000
 export SEQ_LEN=8192
 export BATCH_SIZE=4
 export GRAD_ACCUM=1
+export GRAD_CLIP=0.1
 export LR=3.e-4
 export MIN_LR_RATIO=0.1
 export WARMUP_RATIO=0.05
 export ASYNC_CHECKPOINTING=false
 export BASE_OUTPUT_DIRECTORY="gs://$BUCKET_NAME/model_ckpts/maxtext"
-export DATA_FILES='/home/zephyr/gcs-bucket/datasets/dclm/llama3_64_arrayrecord/*.array_record'
+export DATA_FILES='/home/zephyr/gcs-bucket/datasets/dclm/llama3_64_array_record/*.array_record'
 
-export RUN_NAME="${MODEL_NAME}_L200_seqlen_${SEQ_LEN}_bs_${BATCH_SIZE}_grad_accum_${GRAD_ACCUM}_lr_${LR}_min_lr_ratio_${MIN_LR_RATIO}_warmup_ratio_${WARMUP_RATIO}"
+export RUN_NAME="${MODEL_NAME}_L200_seqlen_${SEQ_LEN}_bs_${BATCH_SIZE}_grad_accum_${GRAD_ACCUM}_grad_clip_${GRAD_CLIP}_lr_${LR}_min_lr_ratio_${MIN_LR_RATIO}_warmup_ratio_${WARMUP_RATIO}_quadratic_warmup"
 
-python -u multihost_runner.py \
+python -u multihost_runner_orig.py \
     --TPU_PREFIX=$TPU_PREFIX \
     --INTERNAL_IP=true \
     --COMMAND="
     export TPU_LOG_DIR=/home/zephyr/tpu_logs
-    sudo docker run \
-        --privileged \
-        --network=host \
-        -v /home/zephyr:/home/zephyr \
-        -v /home/zephyr/.config/gcloud:/root/.config/gcloud \
-        -v /dev:/dev \
-        -v /run:/run \
-        -w /home/zephyr/maxtext \
-        -e PYTHONPATH=/home/zephyr/maxtext \
-        yx3038/maxtext_base_image:latest \
-        bash -c \"
-        export PYTHONPATH=/home/zephyr/maxtext:\$PYTHONPATH
-        python3.10 -u -m MaxText.train MaxText/configs/base.yml \
-            run_name=${RUN_NAME} \
-            base_output_directory=${BASE_OUTPUT_DIRECTORY} \
-            dataset_type=grain \
-            grain_train_files=${DATA_FILES} \
-            grain_file_type='arrayrecord' \
-            grain_worker_count=8 \
-            grain_worker_count_eval=1 \
-            tokenize_train_data=False \
-            tokenize_eval_data=False \
-            max_target_length=${SEQ_LEN} \
-            max_position_embeddings=${SEQ_LEN} \
-            original_max_position_embeddings=${SEQ_LEN} \
-            async_checkpointing=${ASYNC_CHECKPOINTING} \
-            model_name=${MODEL_NAME} \
-            steps=${NUM_STEPS} \
-            per_device_batch_size=${BATCH_SIZE} \
-            gradient_accumulation_steps=${GRAD_ACCUM} \
-            learning_rate=${LR} \
-            cosine_learning_rate_final_fraction=${MIN_LR_RATIO} \
-            warmup_steps_fraction=${WARMUP_RATIO} \
-            checkpoint_period=500 \
-            checkpoint_max_to_keep=1 \
-            use_wandb=False \
-            wandb_project=llm_pruning \
-            wandb_run_name=${RUN_NAME} \
-            packing=false \
-            jax_distributed_initialization_timeout=900
-        \"
+    source ~/maxtext_env/bin/activate
+    export WANDB_API_KEY='7d11bbca76b3081b6bd1efbbcf1572aab26c5d56'
+    python3.10 -u -m MaxText.train MaxText/configs/base.yml \
+        run_name=${RUN_NAME} \
+        base_output_directory=${BASE_OUTPUT_DIRECTORY} \
+        dataset_type=grain \
+        grain_train_files=${DATA_FILES} \
+        grain_file_type='arrayrecord' \
+        grain_worker_count=1 \
+        tokenize_train_data=False \
+        tokenize_eval_data=False \
+        max_target_length=${SEQ_LEN} \
+        async_checkpointing=${ASYNC_CHECKPOINTING} \
+        original_max_position_embeddings=${SEQ_LEN} \
+        model_name=${MODEL_NAME} \
+        steps=${NUM_STEPS} \
+        per_device_batch_size=${BATCH_SIZE} \
+        gradient_accumulation_steps=${GRAD_ACCUM} \
+        gradient_clipping_threshold=${GRAD_CLIP} \
+        learning_rate=${LR} \
+        cosine_learning_rate_final_fraction=${MIN_LR_RATIO} \
+        warmup_steps_fraction=${WARMUP_RATIO} \
+        checkpoint_period=500 \
+        checkpoint_max_to_keep=1 \
+        use_wandb=False \
+        wandb_project=llm_pruning \
+        wandb_run_name=${RUN_NAME} \
+        packing=false
     "
-
-# metrics_file=\"/home/zephyr/gcs-bucket/maxtext/logs/${RUN_NAME}.log\" \
-
-# python3 -m MaxText.train \
-#     MaxText/configs/base.yml \
-#     run_name=runner_pretraining_${idx}\
-#      base_output_directory=${BASE_OUTPUT_DIRECTORY} \
-#      dataset_path=${DATASET_PATH} \
-#      async_checkpointing=${ASYNC_CHECKPOINTING} \
-#      per_device_batch_size=1 \
-#      model_name='llama2-7b' \
-#      ici_context_parallelism=4 \
-#      steps=10 \
-#      per_device_batch_size=1 \
-#      packing=false
