f3abd1cbdd7e91483cad9cf2293dea817fa8a8cd
Yufeng Xu <yx3038@nyu.edu>
2025-09-25 04:21:59 -0400

    added width HF S50 3e-5

---
+++ scripts/llama3.1_4b_width_HF_S50_3e-5_warmup_0.05.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# export TPU_PREFIX=llm-pruning-v6e
+required_vars=(
+    "BUCKET_NAME"
+    "TPU_PREFIX"
+)
+for var in "${required_vars[@]}"; do
+  if [[ -z "${!var:-}" ]]; then
+    echo "[ERROR] $var is not set"
+    exit 1
+  fi
+done
+
+export MODEL_NAME='llama3.1-4b-width'
+export NUM_STEPS=12500
+export SEQ_LEN=8192
+export BATCH_SIZE=2
+export GRAD_ACCUM=4
+export GRAD_CLIP=1.0
+export LR=3.e-5
+export MIN_LR_RATIO=0.1
+export WARMUP_RATIO=0.05
+export ASYNC_CHECKPOINTING=false
+export BASE_OUTPUT_DIRECTORY="gs://$BUCKET_NAME/model_ckpts/maxtext"
+export DATA_FILES='/home/zephyr/gcs-bucket/datasets/dclm/llama3_64_array_record/*.array_record'
+
+export CONVERTED_CHECKPOINT="gs://$BUCKET_NAME/model_ckpts/maxtext/llama3.1-4b-width-orbax/0/items"
+export RUN_NAME="${MODEL_NAME}_HF_S50_seqlen_${SEQ_LEN}_bs_${BATCH_SIZE}_grad_accum_${GRAD_ACCUM}_lr_${LR}_min_lr_ratio_${MIN_LR_RATIO}_warmup_ratio_${WARMUP_RATIO}_test2"
+
+python -u multihost_runner_orig.py \
+    --TPU_PREFIX=$TPU_PREFIX \
+    --COMMAND="
+    export TPU_LOG_DIR=/home/zephyr/tpu_logs
+    export WANDB_API_KEY='7d11bbca76b3081b6bd1efbbcf1572aab26c5d56'
+    source ~/maxtext_env/bin/activate
+    python3.10 -u -m MaxText.train MaxText/configs/base.yml \
+        run_name=${RUN_NAME} \
+        load_parameters_path=${CONVERTED_CHECKPOINT} \
+        base_output_directory=${BASE_OUTPUT_DIRECTORY} \
+        dataset_type=grain \
+        grain_train_files=${DATA_FILES} \
+        grain_file_type='arrayrecord' \
+        grain_worker_count=1 \
+        enable_data_shuffling=False \
+        tokenize_train_data=False \
+        tokenize_eval_data=False \
+        max_target_length=${SEQ_LEN} \
+        async_checkpointing=${ASYNC_CHECKPOINTING} \
+        model_name=${MODEL_NAME} \
+        steps=${NUM_STEPS} \
+        per_device_batch_size=${BATCH_SIZE} \
+        gradient_accumulation_steps=${GRAD_ACCUM} \
+        gradient_clipping_threshold=${GRAD_CLIP} \
+        learning_rate=${LR} \
+        cosine_learning_rate_final_fraction=${MIN_LR_RATIO} \
+        warmup_steps_fraction=${WARMUP_RATIO} \
+        checkpoint_period=250 \
+        checkpoint_max_to_keep=1 \
+        use_wandb=True \
+        wandb_project=llm_pruning \
+        wandb_run_name=${TPU_PREFIX}_${RUN_NAME} \
+        packing=false
+    "
