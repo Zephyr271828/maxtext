c89e13e8693cf1e2e6684a5d7ac764b767cbc3c4
Yufeng Xu <yx3038@nyu.edu>
2025-09-29 08:47:37 -0400

    added llama3.1_1b script

--- MaxText/pyconfig.py
+++ MaxText/pyconfig.py
@@ -322,6 +322,7 @@ def validate_model_name(s: str) -> bool:
       "llama3.1-70b",
       "llama3.1-405b",
       "llama3.3-70b",
+      "llama3.2-1b",
       "mistral-7b",
       "mixtral-8x7b",
       "mixtral-8x22b",
