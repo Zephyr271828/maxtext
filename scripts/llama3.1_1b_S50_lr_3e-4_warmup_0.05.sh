b1c98df569f4611b7034ff8901c53a4529a6eacb
Yufeng Xu <yx3038@nyu.edu>
2025-10-01 08:29:09 -0400

    update config

--- scripts/llama3.1_1b_S50_lr_3e-4_warmup_0.05.sh
+++ scripts/llama3.1_1b_S50_lr_3e-4_warmup_0.05.sh
@@ -58,6 +58,5 @@ python -u multihost_runner_orig.py \
         use_wandb=True \
         wandb_project=llm_pruning \
         wandb_run_name=${TPU_PREFIX}_${RUN_NAME} \
-        packing=false \
-        attention=dot_product
+        packing=false 
     "
