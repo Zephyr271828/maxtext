577ee7461eb0aa6e233e590ae922a08d912f409c
Yufeng Xu <yx3038@nyu.edu>
2025-10-07 03:19:38 -0400

    debugging direct

--- MaxText/llama_mistral_mixtral_orbax_to_hf.py
+++ MaxText/llama_mistral_mixtral_orbax_to_hf.py
@@ -261,7 +261,7 @@ def convert_orbax_hf(hf_model_path, config):
 
 
 def main(argv: Sequence[str]):
-  config = pyconfig.initialize(argv[:-1], init_dist=False)
+  config = pyconfig.initialize(argv[:-1])
   hf_model_path = argv[-1].split("=")[1]
   print(f"Will save converted HuggingFace checkpoint to path = {hf_model_path}")
 
