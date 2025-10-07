d16ceb47eb9d1a60c8f9205b5cfd025743ce3686
Yufeng Xu <yx3038@nyu.edu>
2025-10-07 06:31:40 -0400

    debugging direct

--- MaxText/train.py
+++ MaxText/train.py
@@ -661,25 +661,25 @@ def train_loop(config, recorder, state=None):
       state_to_save = state if not config.use_dpo else _split_dpo_state(state)[0]
       checkpointing.maybe_save_checkpoint(checkpoint_manager, state_to_save, config, data_iterator, step)
 
-      # pseudo_config = pyconfig.HyperParameters(**vars(config))
-      # if config and config.enable_checkpointing:
-      #   if (
-      #     (step % config.checkpoint_period == 0)
-      #     or (config.enable_emergency_checkpoint and step % config.local_checkpoint_period == 0)
-      #   ):
-      #     inner = deepcopy(config._config)
+      pseudo_config = pyconfig.HyperParameters(**vars(config))
+      if config and config.enable_checkpointing:
+        if (
+          (step % config.checkpoint_period == 0)
+          or (config.enable_emergency_checkpoint and step % config.local_checkpoint_period == 0)
+        ):
+          inner = deepcopy(config._config)
           
-      #     print('before', inner.load_paramaters_path)
-      #     inner.load_paramaters_path = f"{config.base_output_directory}/{config.run_name}/checkpoints/{step}/items"
-      #     print('after', inner.load_paramaters_path)
+          print('before', inner.load_paramaters_path)
+          inner.load_paramaters_path = f"{config.base_output_directory}/{config.run_name}/checkpoints/{step}/items"
+          print('after', inner.load_paramaters_path)
           
-      #     print('before', inner.run_name)
-      #     inner.run_name = f"direct_{config.run_name}"
-      #     print('after', inner.run_name)
+          print('before', inner.run_name)
+          inner.run_name = f"direct_{config.run_name}"
+          print('after', inner.run_name)
           
-      #     inner.force_unroll = True
-      #     pseudo_config = pyconfig.HyperParameters(inner)
-      #     generate_decode_checkpoint(pseudo_config, step=step)
+          inner.force_unroll = True
+          pseudo_config = pyconfig.HyperParameters(inner)
+          generate_decode_checkpoint(pseudo_config, step=step)
 
       if config.dump_hlo and step == (config.dump_step if config.dump_step >= 0 else start_step):
         jax.block_until_ready(state)  # Ensure compilation has finished.
