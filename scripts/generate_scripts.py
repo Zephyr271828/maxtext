e79cb2b07f68e8fa182166aa51c5e84291794259
Yufeng Xu <yx3038@nyu.edu>
2025-10-16 13:55:51 -0400

    added scripts

--- scripts/generate_scripts.py
+++ scripts/generate_scripts.py
@@ -229,7 +229,7 @@ if __name__ == "__main__":
             )
             
     for load_path in ["model_ckpts/maxtext/llama3.1_8b_L200_unstructured_0.5/checkpoints/0/items", "model_ckpts/maxtext/llama3.1_8b_L200_unstructured_0.5_reinit/checkpoints/0/items"]:
-        for lr in [1e-6, 3e-6, 1e-5, 3e-5, 1e-4, 3e-4]:
+        for lr in [1e-6, 3e-6, 1e-5, 3e-5, 1e-4, 3e-4, 1e-3]:
             generate_script(
                 model_name="llama3.1-8b",
                 lr=lr,
