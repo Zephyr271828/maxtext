ff726421400f629876f3242db3b102e684821611
Yufeng Xu <yx3038@nyu.edu>
2025-10-29 14:23:16 -0400

    update launch_convert

--- scripts/launch_convert.sh
+++ scripts/launch_convert.sh
@@ -6,6 +6,7 @@ host_id=$(python -c "import socket; print(socket.gethostname())" | awk -F'-' '{p
 
 model=llama3.1-1b
 step=$((host_id * 500 + 500))
+# orbax_ckpt_name="llama3.1-1b_S50_seqlen_8192_bs_2_grad_accum_4_lr_3e-4_min_lr_ratio_0.1_warmup_ratio_0.05"
 orbax_ckpt_name="llama3.1-1b_S50_seqlen_8192_bs_2_grad_accum_4_lr_3e-4_min_lr_ratio_0.1_warmup_ratio_0.05"
 direct_run_name="${orbax_ckpt_name}_step_${step}"
 bash scripts/convert.sh gen_param_ckpt \
