efb5a838737a2820e4118cda68f6b96ef0909184
Yufeng Xu <yx3038@nyu.edu>
2025-10-28 22:42:28 -0400

    enable data loader shuffling and monkey patched initializer

--- scripts/generate_scripts.py
+++ scripts/generate_scripts.py
@@ -96,7 +96,7 @@ def generate_script(
             start_from_file_index={start_from_file_index} \\
             grain_file_type='arrayrecord' \\
             grain_worker_count=1 \\
-            enable_data_shuffling=False \\
+            enable_data_shuffling=True \\
             tokenize_train_data=False \\
             tokenize_eval_data=False \\
             max_target_length=${{SEQ_LEN}} \\
--- scripts/llama3.1-4b-width_S50.sh
+++ scripts/llama3.1-4b-width_S50.sh
@@ -48,7 +48,7 @@ python -u multihost_runner_orig.py \
         start_from_file_index=0 \
         grain_file_type='arrayrecord' \
         grain_worker_count=1 \
-        enable_data_shuffling=False \
+        enable_data_shuffling=True \
         tokenize_train_data=False \
         tokenize_eval_data=False \
         max_target_length=${SEQ_LEN} \
