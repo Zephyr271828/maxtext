269d68c1ca51ad845f8adde89b8e9f2424bf418e
Yufeng Xu <yx3038@nyu.edu>
2025-10-30 14:34:31 -0400

    update launch_convert

--- scripts/launch_convert.sh
+++ scripts/launch_convert.sh
@@ -4,10 +4,16 @@ set -euo pipefail
 
 host_id=$(python -c "import socket; print(socket.gethostname())" | awk -F'-' '{print $NF}')
 
+for arg in "$@"; do
+  case $arg in
+    --model=*) MODEL="${arg#*=}" ;;
+    --orbax_ckpt_name=*) ORBAX_CKPT_NAME="${arg#*=}" ;;
+    *) echo "[WARN] Unknown arg $arg" ;;
+  esac
+done
+
 model=llama3.1-1b
 step=$((host_id * 500 + 500))
-# orbax_ckpt_name="llama3.1-1b_S50_seqlen_8192_bs_2_grad_accum_4_lr_3e-4_min_lr_ratio_0.1_warmup_ratio_0.05"
-orbax_ckpt_name="llama3.1-1b_S50_seqlen_8192_bs_2_grad_accum_4_lr_3e-4_min_lr_ratio_0.1_warmup_ratio_0.05"
 direct_run_name="${orbax_ckpt_name}_step_${step}"
 bash scripts/convert.sh gen_param_ckpt \
     --model=${model} \
