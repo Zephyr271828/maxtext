266fa49c9db0365e1b8dd6d3e5a227353d88f52d
Yufeng Xu <yx3038@nyu.edu>
2025-11-04 09:57:38 -0500

    update launch_compare

--- scripts/launch_compare.sh
+++ scripts/launch_compare.sh
@@ -14,10 +14,10 @@ done
 
 host_id=$(python -c "import socket; print(socket.gethostname())" | awk -F'-' '{print $NF}')
 
-
+offset=${offset:-0}
 model=${model:-"llama3.1-1b"}
 hf_model_name=${hf_model_name:-"llama3.1_1b_seq_8192_bs_2_grad_accum_8_steps_12500_lr_0.0003_minlr_ratio_0.1_warmup_0.05"}
-step=$(( host_id + offset ))
+step=$((host_id + offset))
 orbax_ckpt_name=${orbax_ckpt_name:-"llama3.1-1b_S50_seqlen_8192_bs_1_grad_accum_1_lr_0.0003_min_lr_ratio_0.1_warmup_ratio_0.05"}
 direct_run_name="${orbax_ckpt_name}_step_${step}"
 bash scripts/convert.sh gen_param_ckpt \
