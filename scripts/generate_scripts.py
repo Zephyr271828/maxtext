9e75261981387c7101bbb2b4eb29f86510af00b6
Yufeng Xu <yx3038@nyu.edu>
2026-01-20 04:02:10 -0500

    added llama3-2b-width and llama3-3b-width

--- scripts/generate_scripts.py
+++ scripts/generate_scripts.py
@@ -184,7 +184,11 @@ if __name__ == "__main__":
                 # output_path=args.output_path,
             )
             
-    for model_name in ["llama3.1-440m", "llama3.1-1b", "llama3.1-1.5b-depth", "llama3.1-2b-depth", "llama3.1-3b-depth"]:
+    for model_name in [
+        "llama3.1-440m", "llama3.1-1b", 
+        "llama3.1-1.5b-depth", "llama3.1-2b-depth", "llama3.1-3b-depth",
+        "llama3.1-2b-width", "llama3.1-3b-width",
+    ]:
         for num_steps in [12500, 62500]:
             generate_script(
                 model_name=model_name,
