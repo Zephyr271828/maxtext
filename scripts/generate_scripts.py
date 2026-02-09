01fdf6dc895faf6d4b12d5e0a8e4d41a1bf2f006
Zephyr271828 <yx3038@nyu.edu>
2026-02-10 02:46:53 +0800

    add llama2 2.7b scripts

--- scripts/generate_scripts.py
+++ scripts/generate_scripts.py
@@ -368,3 +368,13 @@ if __name__ == "__main__":
                 # load_parameters_path="model_ckpts/maxtext/Llama-3.1-8B_depth_task_wikitext_nlayers_16_calib_size_1024_seqlen_8192_fewshot_0/checkpoints/0/items",
                 # pretrain_tokens="Meta",
             )
+            
+    for model_name in ["llama2-2.7b"]:
+        for num_steps in [2500, 7500, 12500, 62500, 125000]:
+            generate_script(
+                model_name=model_name,
+                num_steps=num_steps,
+                data_files="/home/zephyr/gcs-bucket/datasets/dclm/llama2_array_record_with_special_tokens_1T/*.array_record",
+                # load_parameters_path="model_ckpts/maxtext/Llama-3.1-8B_depth_task_wikitext_nlayers_16_calib_size_1024_seqlen_8192_fewshot_0/checkpoints/0/items",
+                # pretrain_tokens="Meta",
+            )
