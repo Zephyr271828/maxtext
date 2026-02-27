f66218f1bd725fdbc3d4793faa6d304fa34aa73c
Zephyr271828 <yx3038@nyu.edu>
2026-02-27 13:17:26 +0800

    modified multihost_runner

--- multihost_runner_orig.py
+++ multihost_runner_orig.py
@@ -198,7 +198,7 @@ def delete_old_run_dirs(slices):
           cur_slice.name,
           f"--worker={worker_num}",
           "--command", delete_cmd,
-          "--ssh-key-file=~/.ssh/id_rsa",
+          # "--ssh-key-file=~/.ssh/google_compute_engine",
           "--strict-host-key-checking=no",
           f"--project={args.PROJECT}",
           f"--zone={args.ZONE}",
@@ -242,7 +242,9 @@ def scps(slices, run_name_dir, zip_name):
     for worker_num in range(cur_slice.num_workers):
       command = [
           "gcloud", "compute", "tpus", "tpu-vm", "scp", f"--worker={worker_num}", zip_path,
-          f"{cur_slice.name}:~/", "--ssh-key-file=~/.ssh/id_rsa", "--strict-host-key-checking=no", f"--project={args.PROJECT}", f"--zone={args.ZONE}"
+          f"{cur_slice.name}:~/", 
+          # "--ssh-key-file=~/.ssh/google_compute_engine", 
+          "--strict-host-key-checking=no", f"--project={args.PROJECT}", f"--zone={args.ZONE}"
       ]
       if args.INTERNAL_IP:
         command.append("--internal-ip")
@@ -285,7 +287,9 @@ def execute_main_command(main_command, slices, local_log_dir, zip_name):
       remote_command_list_str = " && ".join(remote_command_list)
       gcloud_command=[
           "gcloud", "alpha", "compute", "tpus", "tpu-vm", "ssh", cur_slice.name, f"--worker={worker_num}",
-          "--command", remote_command_list_str, "--ssh-key-file=~/.ssh/id_rsa", "--strict-host-key-checking=no",
+          "--command", remote_command_list_str, 
+          # "--ssh-key-file=~/.ssh/google_compute_engine",
+          "--strict-host-key-checking=no",
           f"--project={args.PROJECT}", f"--zone={args.ZONE}"]
       if args.INTERNAL_IP:
         gcloud_command.append("--internal-ip")
